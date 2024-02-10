f6d04b6181542811611caf7abb33383db9b67b49
vedant Rajput <rajputvedant213@gmail.com>
2024-02-11 00:28:24 +0530

    clock comp icons done!!!

--- src/app/clock/digitalclock/digitalclock.component.ts
+++ src/app/clock/digitalclock/digitalclock.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-digitalclock',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './digitalclock.component.html',
   styleUrl: './digitalclock.component.css'
 })
 export class DigitalclockComponent {
-  
+
 }
--- src/app/clock/timer/timer.component.ts
+++ src/app/clock/timer/timer.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-timer',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './timer.component.html',
   styleUrl: './timer.component.css'
 })
