e7da9c83e02d85e16032a28b899ce3078e79a744
vedant Rajput <rajputvedant213@gmail.com>
2024-02-20 22:51:57 +0530

    to-do list completed

--- src/app/to-do/to-do.component.ts
+++ src/app/to-do/to-do.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NgModel } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { Task } from './task.interface';
-import { OutgoingMessage } from 'http';
+import { TodoService } from './services/todo.service';
+import { userInfo } from 'os';
 
 @Component({
   selector: 'app-to-do',
@@ -14,11 +14,21 @@ import { OutgoingMessage } from 'http';
   styleUrl: './to-do.component.css'
 })
 export class ToDoComponent implements OnInit {
+
   tasks: Task[] = [];
   newTask: string = '';
-  constructor() { }
+
+  constructor(private localStorage: TodoService) { }
+
   ngOnInit(): void {
+
+    const dataRetrive = this.localStorage.getData('userData');
+    if (dataRetrive) {
+      this.tasks = dataRetrive;
+    }
+
   }
+
   addTask() {
     if (this.newTask.trim() !== '') {
       const newTask: Task = {
@@ -26,6 +36,9 @@ export class ToDoComponent implements OnInit {
         completed: false
       };
       this.tasks.push(newTask);
+
+      this.localStorage.saveData('userData', this.tasks);
+
       this.newTask = '';
     }
   }
@@ -34,11 +47,15 @@ export class ToDoComponent implements OnInit {
     const taskIndex = this.tasks.indexOf(task);
     if (taskIndex !== -1) {
       this.tasks.splice(taskIndex, 1);
+      this.localStorage.saveData('userData', this.tasks)
     }
-  }
-  toggleCompleted(task: Task) {
-    task.completed = !task.completed;
+
   }
 
-  
+  toggleCompleted(task: Task) {
+    task.completed = !task.completed;
+    this.localStorage.saveData('userData', this.tasks);
+  }
+
+
 }
